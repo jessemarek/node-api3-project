7b1b432d212956b9afa1e0df58c8d5cb479ab09c
unknown <dethgrenade@gmail.com>
2020-06-03 12:04:56 -0700

    initial setup of server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const server = express();
+
+const postRouter = require('./posts/postRouter')
+const userRouter = require('./users/userRouter')
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +10,8 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) { }
+
+server.use(express.json())
 
 module.exports = server;
