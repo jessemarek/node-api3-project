fe3558e84b850a1e65b684390ed6351c5571a39d
unknown <dethgrenade@gmail.com>
2020-06-03 12:40:14 -0700

    logger and validateUserId working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,19 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
+const Posts = require('./postDb')
+
 router.get('/', (req, res) => {
   // do your magic!
+  Posts.get()
+    .then(posts => {
+      if (posts.length) {
+        res.status(200).json(posts)
+      }
+      else {
+        res.status(404).json({ message: "no posts found" })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "error getting posts from the server" })
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validatePostId, (req, res) => {
   // do your magic!
 });
 
@@ -22,6 +37,7 @@ router.put('/:id', (req, res) => {
 
 function validatePostId(req, res, next) {
   // do your magic!
+  next()
 }
 
 module.exports = router;
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const router = express.Router();
 
+const Users = require('./userDb')
+
 router.post('/', (req, res) => {
   // do your magic!
+
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -12,9 +15,22 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+    .then(users => {
+      if (users.length) {
+        res.status(200).json(users)
+      }
+      else {
+        res.status(404).json({ message: "no users found" })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ message: "error getting users from the server" })
+    })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
 });
 
@@ -34,6 +50,20 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const { id } = req.params
+  Users.getById(id)
+    .then(user => {
+      if (user) {
+        req.user = user
+        next()
+      }
+      else {
+        res.status(400).json({ message: "invalid user id" })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "error finding user by id on the server" })
+    })
 }
 
 function validateUser(req, res, next) {
